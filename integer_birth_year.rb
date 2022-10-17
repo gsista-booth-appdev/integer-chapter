050b1b4d03df5cdfea20e8402c1eaa50535a95ec
gsista-booth <gsista@chicagobooth.edu>
2022-10-17 01:57:20 +0000

    birth year commit

--- integer_birth_year.rb
+++ integer_birth_year.rb
@@ -1,9 +1,13 @@
 # Ask for a person’s age, and then calculate and display what year they were born in.
-# 
+#
 # Input:
 #   80
-# 
+#
 # Key output:
 #   "Wow, you were born in 1940. You're old!"
 
 p "How old are you?"
+
+age = gets.to_i
+
+p "Wow, you were born in " + (2022 - age).to_s + ". You're old!"
